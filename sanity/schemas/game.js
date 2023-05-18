b9f1c53b3719a5536e88db1758042a1640344816
Mohammed Ghazwan Almilhim <almilhimmohammed52@gmail.com>
2023-05-18 17:39:09 +0200

    update

--- sanity/schemas/game.js
+++ sanity/schemas/game.js
@@ -1,55 +1,48 @@
 export default {
-    name: 'game',
-    title: 'Game',
-    type: 'document',
-    fields: [
-      {
-        name: 'title',
-        title: 'Title',
-        type: 'string',
-      },
-      {
-        name: 'slug',
-        title: 'Slug',
-        type: 'slug',
-        options: {
-          source: 'title',
-          slugify: input => input
-                         .toLowerCase()
-                         .replace(/\s+/g, '-')
-                         .slice(0, 200)
-        }
-      },
-      {
-        name: 'apiid',
-        title: 'API-id',
-        type: 'number',
-      },
-      {
-        name: 'timerspilt',
-        title: 'Timer spilt',
-        type: 'string',
-      },
-      {
-        name: 'sjangere',
-        title: 'Sjangere',
-        type: 'array',
-        of: [{ type: 'reference', to: { type: 'genre' } }],
-      },
-      {
-        name: 'bilde',
-        title: 'Bilde',
-        type: 'url',
-      },
-      {
-        name: 'released',
-        title: 'Released date',
-        type: 'date',
-      },
-      {
-        name: 'steamlink',
-        title: 'Steam Link',
-        type: 'url',
-      },
-    ],
-  }
+  name: 'game',
+  title: 'Game',
+  type: 'document',
+  fields: [
+    {
+      name: 'title',
+      title: 'Title',
+      type: 'string',
+    },
+    {
+      name: 'slug',
+      title: 'Slug',
+      type: 'slug',
+    },
+    {
+      name: 'apiid',
+      title: 'API-id',
+      type: 'number',
+    },
+    {
+      name: 'hoursplayed',
+      title: 'Hours Played',
+      type: 'string',
+    },
+    {
+      name: 'genres',
+      title: 'Genres',
+      type: 'array',
+      of: [{ type: 'reference', to: { type: 'genre' } }],
+    },
+    {
+      name: 'img',
+      title: 'Image',
+      type: 'image',
+    },
+    {
+      name: 'released',
+      title: 'Released date',
+      type: 'date',
+    },
+    {
+      name: 'steamlink',
+      title: 'Steam Link',
+      type: 'url',
+    },
+  ],
+}
