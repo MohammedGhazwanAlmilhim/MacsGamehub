96c878e47eeef5ad50e4d9a6c9ca90915194ff84
Mohammed Ghazwan Almilhim <112077465+mohammga@users.noreply.github.com>
2023-05-18 17:39:09 +0200

    update

--- sanity/schemas/genre.js
+++ sanity/schemas/genre.js
@@ -1,12 +1,12 @@
 export default {
-    name: 'genre',
-    title: 'Genre',
-    type: 'document',
-    fields: [
-      {
-        name: 'navn',
-        title: 'Navn',
-        type: 'string',
-      },
-    ],
-  }
+  name: 'genre',
+  title: 'Genre',
+  type: 'document',
+  fields: [
+    {
+      name: 'name',
+      title: 'Name',
+      type: 'string',
+    },
+  ],
+}
--- sanity/schemas/user.js
+++ sanity/schemas/user.js
@@ -1,23 +1,23 @@
 export default {
-    name: 'user',
-    title: 'User',
-    type: 'document',
-    fields: [
-      {
-        name: 'name',
-        title: 'Name',
-        type: 'string',
-      },
-      {
-        name: 'email',
-        title: 'Email',
-        type: 'string',
-      },
-      {
-        name: 'favoriteGames',
-        title: 'Favorite Games',
-        type: 'array',
-        of: [{ type: 'reference', to: { type: 'game' } }],
-      },
-    ],
-  };
+  name: 'user',
+  title: 'User',
+  type: 'document',
+  fields: [
+    {
+      name: 'name',
+      title: 'Name',
+      type: 'string',
+    },
+    {
+      name: 'email',
+      title: 'Email',
+      type: 'string',
+    },
+    {
+      name: 'favouriteGames',
+      title: 'Favourite Games',
+      type: 'array',
+      of: [{ type: 'reference', to: { type: 'game' } }],
+    },
+  ],
+};
